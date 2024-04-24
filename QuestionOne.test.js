1cbea5fc710073df60676972827647931524e030
aondofe014704 <songujack@gmail.com>
2024-04-24 17:11:42 +0100

    javascript testing Drills

--- QuestionOne.test.js
+++ QuestionOne.test.js
@@ -2,21 +2,21 @@
 
 let {highestScores, addFiveToScores} = require("./QuestionOne.js");
 
-test("filter highest scores", () => {
+test("FilterHighestScores", () => {
     let testScores = [20, 30, 60, 45, 95, 80, 25, 75, 60, 90];
     let result = [95, 80, 75, 90];
     expect(result).toEqual([95, 80, 75, 90])
 
 })
 
-test("improve student scores by 5", () => {
+test("ImproveStudentScoresByFive(5)", () => {
     let studentScores = [85, 92, 78, 88, 95];
     let newScores = [90, 97, 83, 80, 95];
     expect(newScores).toEqual([90, 97, 83, 80, 95]);
 
 })
 
-test("square of numbers in a list", () => {
+test("squareOfNumbersInAList", () => {
     let listNumbers = [2, 4, 6, 8, 10];
     let result = [4, 16, 36, 64, 100];
     expect(result).toEqual([4, 16, 36, 64, 100]);
